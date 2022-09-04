855ab2a1f93bc65a06b3ff0bcf46aaded1a74432
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-04 13:20:33 +0200

    modify App.js

--- React/src/components/App.js
+++ React/src/components/App.js
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import "../css/App.css";
 import ListContacts from "./ListContacts";
 import CreateContact from './CreateContact';
 import * as ContactsAPI from "../utils/ContactsAPI"
-import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
   let navigate = useNavigate();
